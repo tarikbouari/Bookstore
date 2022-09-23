b867981838647accb7ed4861b705458f66565480
Rikstone <tarikbouari@gmail.com>
2022-09-23 13:06:28 -0700

    update Addbook component

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from './AddBook.module.css';
-// import 'bootstrap/dist/css/bootstrap.css';
+
 const AddBook = () => (
   <div className={style.Books}>
     <form>
