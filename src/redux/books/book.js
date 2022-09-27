e3fa9658e3f523981e0e42adf7ad75d3b897fa52
Rikstone <tarikbouari@gmail.com>
2022-09-27 07:35:40 -0700

    create book action and reducers

---
+++ src/redux/books/book.js
@@ -0,0 +1,45 @@
+
+const initialState = [];
+
+let nextTodoId = 0;
+const ADD_BOOK = 'redux/books/book/ADD_BOOK';
+const RMV_BOOK = 'redux/books/book/RMV_BOOK';
+
+ export const enterBook = (titleBook,author) => {
+   return {
+    type: ADD_BOOK,
+    id : nextTodoId++,
+    titleBook: titleBook,
+    author: author
+   } 
+}
+
+ export const removeBook = (id) => {
+   return {
+      type: RMV_BOOK,
+      id
+   }
+}
+
+ export const bookReducer = (state = initialState, action) =>{
+    switch(type.action){
+        case ADD_BOOK : return [
+            ...state,
+            {
+                title: action.titleBook,
+                author: action.author,
+                id : action.id
+
+            }
+        ]
+        case RMV_BOOK :
+             return state.filter( item =>
+                 item.id !== action.id
+                 )
+        
+         default: return state
+        }
+        
+    }
+
+
