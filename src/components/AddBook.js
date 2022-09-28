e793f64ad15601e48a3af3f29b91296d63733961
Rikstone <tarikbouari@gmail.com>
2022-09-28 04:48:47 -0700

    send book to the store

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -7,19 +7,33 @@ const AddBook = () => {
   const dispatch = useDispatch();
   const [stateForm, setStateForm] = useState({title:"", author:""});
   
-  const ChangeFormState = (e) => {
-    e.preventDefault;
+  const changeFormState = (e) => {
+    e.preventDefault();
     setStateForm({...stateForm, [ e.target.name]: e.target.value})
+  } 
+
+  const sendBookState = (e) => {
+    let idBook =0;
+    e.preventDefault();
+    if(!stateForm.title.trim() || !stateForm.author.trim()) return alert ("Add title and Author");
+    
+    const book = {
+      title: stateForm.title,
+      author:stateForm.author,
+      id: idBook+1
+    }
+    dispatch(enterBook(book))
+    setStateForm({title:'',author:''})
   } 
 
   return (
     <div className={style.Books}>
-      <form onSubmit={() => dispatch(enterBook())}>
+      <form onSubmit={sendBookState()}>
         <span name="book" className="my-3"> ADD NEW BOOK</span>
         <div className={style.BoxInput}>
-          <input type="text" placeholder="Book title" name= "title" />
-          <input type="text" placeholder="Author" name= "author" />
-          <button type="submit" className="btn btn-primary">Add </button>
+          <input type="text" placeholder="Book title" name= "title"  onChange={changeFormState()}  value={stateForm.title}/>
+          <input type="text" placeholder="Author" name= "author" value={stateForm.author}/>
+          <button type="submit" className="btn btn-primary">ADD BOOK</button>
         </div>
       </form>
     </div>
