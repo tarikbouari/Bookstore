7a935ac819a840092a66c1064445b8c22795712d
Rikstone <tarikbouari@gmail.com>
2022-09-28 08:16:27 -0700

    fetch data from the redux store

--- src/components/Categorie.js
+++ src/components/Categorie.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
+const Categories = () => {
+  const dispatch = useDispatch();
+  const displayStatus = useSelector((state) => state.categories)
+  
+  const checkBookStatus = () =>{
+    dispatch(checkStatus());
+  }
+  return (
   <div>
-    <button type="submit" className="btn btn-primary mx-5 w-25">
+    <button type="submit" className="btn btn-primary mx-5 w-25" onClick={checkBookStatus}>
       Check Status
     </button>
+    <h5>{displayStatus}</h5>
   </div>
-);
+  )
+}
 
 export default Categories;
