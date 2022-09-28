202d79e883bcb383c2514e04971f93b53704f86f
Rikstone <tarikbouari@gmail.com>
2022-09-28 04:05:03 -0700

    update App.s

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Nav from './components/Nav';
 import Categories from './components/Categorie';
 import DisplayBook from './components/DisplayBook';
+import store from './redux/configureStore';
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Routes>
-        <Route path="/" element={<DisplayBook />} />
-        <Route path="Categories" element={<Categories />} />
-      </Routes>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<DisplayBook />} />
+          <Route path="Categories" element={<Categories />} />
+        </Routes>
+      </div>
+    </Provider>
+
   );
 }
 
