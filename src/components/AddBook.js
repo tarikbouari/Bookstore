559cab2cc41f658215b84c979d6e913c825db293
Rikstone <tarikbouari@gmail.com>
2022-09-28 07:21:39 -0700

    import user identifier

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,38 +1,39 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
 import style from './AddBook.module.css';
 import { enterBook } from '../redux/books/books';
 
 const AddBook = () => {
   const dispatch = useDispatch();
-  const [stateForm, setStateForm] = useState({title:"", author:""});
-  
+  const [stateForm, setStateForm] = useState({ title: '', author: '' });
+
   const changeFormState = (e) => {
     e.preventDefault();
-    setStateForm({...stateForm, [ e.target.name]: e.target.value})
-  } 
+    setStateForm({ ...stateForm, [e.target.name]: e.target.value });
+  };
 
   const sendBookState = (e) => {
-    let idBook =0;
     e.preventDefault();
-    if(!stateForm.title.trim() || !stateForm.author.trim()) return alert ("Add title and Author");
-    
+    if (!stateForm.title.trim() || !stateForm.author.trim()) return;
+
     const book = {
       title: stateForm.title,
-      author:stateForm.author,
-      id: idBook+1
-    }
-    dispatch(enterBook(book))
-    setStateForm({title:'',author:''})
-  } 
+      author: stateForm.author,
+      id: uuidv4(),
+    };
 
+    dispatch(enterBook(book));
+    console.log(book);
+    setStateForm({ title: '', author: '' });
+  };
   return (
     <div className={style.Books}>
-      <form onSubmit={sendBookState()}>
+      <form onSubmit={sendBookState}>
         <span name="book" className="my-3"> ADD NEW BOOK</span>
         <div className={style.BoxInput}>
-          <input type="text" placeholder="Book title" name= "title"  onChange={changeFormState()}  value={stateForm.title}/>
-          <input type="text" placeholder="Author" name= "author" value={stateForm.author}/>
+          <input type="text" placeholder="Book title" name="title" onChange={changeFormState} value={stateForm.title} />
+          <input type="text" placeholder="Author" name="author" onChange={changeFormState} value={stateForm.author} />
           <button type="submit" className="btn btn-primary">ADD BOOK</button>
         </div>
       </form>
