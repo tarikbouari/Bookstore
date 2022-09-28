c6d40bd17bba9ead57233abc72300497b5fdeb52
Rikstone <tarikbouari@gmail.com>
2022-09-28 04:06:07 -0700

    import useSeletor

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import style from './AddBook.module.css';
+import { enterBook } from '../redux/books/books';
 
-const AddBook = () => (
-  <div className={style.Books}>
-    <form>
-      <span name="book" className="my-3"> ADD NEW BOOK</span>
-      <div className={style.BoxInput}>
-        <input type="text" placeholder="Book title" />
-        <input type="text" placeholder="Author" />
-        <button type="button" className="btn btn-primary">Add </button>
-      </div>
-    </form>
-  </div>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
+  return (
+    <div className={style.Books}>
+      <form onSubmit={() => dispatch(enterBook())}>
+        <span name="book" className="my-3"> ADD NEW BOOK</span>
+        <div className={style.BoxInput}>
+          <input type="text" placeholder="Book title" />
+          <input type="text" placeholder="Author" />
+          <button type="submit" className="btn btn-primary">Add </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
