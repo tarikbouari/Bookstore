12f6b164a9a60c04aeced1bff817ff029fab9e04
Rikstone <tarikbouari@gmail.com>
2022-09-28 04:20:38 -0700

    add ChangeState function

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import style from './AddBook.module.css';
 import { enterBook } from '../redux/books/books';
 
 const AddBook = () => {
   const dispatch = useDispatch();
+  const [stateForm, setStateForm] = useState({title:"", author:""});
+  
+  const ChangeFormState = (e) => {
+    e.preventDefault;
+    setStateForm({...stateForm, [ e.target.name]: e.target.value})
+  } 
+
   return (
     <div className={style.Books}>
       <form onSubmit={() => dispatch(enterBook())}>
         <span name="book" className="my-3"> ADD NEW BOOK</span>
         <div className={style.BoxInput}>
-          <input type="text" placeholder="Book title" />
-          <input type="text" placeholder="Author" />
+          <input type="text" placeholder="Book title" name= "title" />
+          <input type="text" placeholder="Author" name= "author" />
           <button type="submit" className="btn btn-primary">Add </button>
         </div>
       </form>
