1dbb1494acfff16c4fcc0cba5341c862534a98c3
Rikstone <tarikbouari@gmail.com>
2022-09-29 16:08:04 -0700

    add bookreducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,17 +1,19 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from 'axios';
 
-
 const ADD_BOOK = "bookstore/books/ADD_BOOK";
 const RMV_BOOK = "bookstore/books/RMV_BOOK";
 const GET_BOOK = "bookstore/books/GET_BOOKS"
 
+
 export const bookReducer = (state = [], action) => {
   switch (action.type) {
-    case ADD_BOOK:
-      return [...state, action.payload];
-    case RMV_BOOK:
-      return state.filter((item) => item.id !== action.id);
+    case 'bookstore/books/GET_BOOK/SUCCESS':
+      return action.payload.book
+    case 'bookstore/books/ADD_BOOK/SUCCESS':
+      return[...state, action.payload.book]
+    case 'bookstore/books/RMV_BOOK/SUCCESS':
+      return [...state.filter((item) => item[0] !== action.payload.id);
 
     default:
       return state;
@@ -25,5 +27,5 @@ export const enterBook = (book) => ({
 
 export const removeBook = (id) => ({
   type: RMV_BOOK,
-  id,
+  payload:id,
 });
