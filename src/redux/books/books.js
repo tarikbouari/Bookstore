437647f1ccf0b7d78cc467645ffbb9f2da554f0c
Rikstone <tarikbouari@gmail.com>
2022-09-29 17:27:41 -0700

    add async function

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 const ADD_BOOK = "bookstore/books/ADD_BOOK";
 const RMV_BOOK = "bookstore/books/RMV_BOOK";
 const GET_BOOK = "bookstore/books/GET_BOOKS"
-
-
+urlApi = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/${key}/books`;
+key= 'HNRWWKcb5eZvN9Tn2PtR';
 export const bookReducer = (state = [], action) => {
   switch (action.type) {
     case 'bookstore/books/GET_BOOK/SUCCESS':
@@ -20,12 +20,36 @@ export const bookReducer = (state = [], action) => {
   }
 };
 
-export const enterBook = (book) => ({
-  type: ADD_BOOK,
-  payload: book,
+export const getBooks = createAsyncThunk(GET_BOOK, async() => {
+  const res = await axios.get(urlApi);
+  return {books: Object.entries(res.data)}
 });
 
-export const removeBook = (id) => ({
-  type: RMV_BOOK,
-  payload:id,
-});
+export const addBook = createAsyncThunk(ADD_BOOK, async (book) => {
+  const res = await axios.post(urlApi, 
+    {
+      item_id: book.item_id,
+      title: book.title,
+      author: book.author,
+      category:book.category,
+
+    })
+
+    return {
+      book:[
+        book.item_id,
+        [{
+          author:book.author,
+          title:book.title,
+          category: book.category,
+
+        }],
+        
+      ]
+    }
+})
+
+export const removeBook = createAsyncThunk(RMV_BOOK, async(id) => {
+    const res = await axios.delete(urlApi)
+    return {id}
+})
