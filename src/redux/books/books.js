c49f708a5fbcc5c8c19416533ef8fc903d64daba
Rikstone <tarikbouari@gmail.com>
2022-09-30 08:57:34 +0000

    update reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,55 +1,58 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const ADD_BOOK = "bookstore/books/ADD_BOOK";
-const RMV_BOOK = "bookstore/books/RMV_BOOK";
-const GET_BOOK = "bookstore/books/GET_BOOKS"
-urlApi = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/${key}/books`;
-key= 'HNRWWKcb5eZvN9Tn2PtR';
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const RMV_BOOK = 'bookstore/books/RMV_BOOK';
+const GET_BOOK = 'bookstore/books/GET_BOOKS';
+const key = 'HNRWWKcb5eZvN9Tn2PtR';
+const urlApi = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${key}/books`;
+
 export const bookReducer = (state = [], action) => {
   switch (action.type) {
-    case 'bookstore/books/GET_BOOK/SUCCESS':
-      return action.payload.book
-    case 'bookstore/books/ADD_BOOK/SUCCESS':
-      return[...state, action.payload.book]
-    case 'bookstore/books/RMV_BOOK/SUCCESS':
-      return [...state.filter((item) => item[0] !== action.payload.id);
+    case 'bookstore/books/GET_BOOK/fulfilled':
+      return action.payload.book;
+    case 'bookstore/books/ADD_BOOK/fulfilled':
+      return [...state, action.payload.book];
+    case 'bookstore/books/RMV_BOOK/fulfilled':
+      return [...state.filter((item) => item[0] !== action.payload.id)];
 
     default:
       return state;
   }
 };
 
-export const getBooks = createAsyncThunk(GET_BOOK, async() => {
+export const getBooks = createAsyncThunk(GET_BOOK, async () => {
   const res = await axios.get(urlApi);
-  return {books: Object.entries(res.data)}
+  console.log(res);
+  return { book: Object.entries(res.data) };
+  
 });
 
-export const addBook = createAsyncThunk(ADD_BOOK, async (book) => {
-  const res = await axios.post(urlApi, 
+export const addBook = createAsyncThunk(ADD_BOOK, async (booke) => {
+  await axios.post(urlApi,
     {
-      item_id: book.item_id,
-      title: book.title,
-      author: book.author,
-      category:book.category,
+      item_id: booke.item_id,
+      title: booke.title,
+      author: booke.author,
+      category: booke.category,
 
-    })
+    });
 
-    return {
-      book:[
-        book.item_id,
-        [{
-          author:book.author,
-          title:book.title,
-          category: book.category,
+  return {
+    book: [
+      booke.item_id,
+      [{
+        author: booke.author,
+        title: booke.title,
+        category: booke.category,
 
-        }],
-        
-      ]
-    }
-})
+      }],
 
-export const removeBook = createAsyncThunk(RMV_BOOK, async(id) => {
-    const res = await axios.delete(urlApi)
-    return {id}
-})
+    ],
+  };
+});
+
+export const removeBook = createAsyncThunk(RMV_BOOK, async (id) => {
+  await axios.delete(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${key}/books/${id}`);
+  return { id };
+});
