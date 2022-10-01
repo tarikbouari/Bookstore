d28bf0c201935b7646c2eeafa886a4bc5493a307
Rikstone <tarikbouari@gmail.com>
2022-10-01 10:43:47 +0000

    Create Chapter component

--- src/components/Chapter.js
+++ src/components/Chapter.js
@@ -2,14 +2,14 @@ import React from 'react';
 import style from './chapter.module.css';
 
 const Chapter = () => (
-  <div>
+  <div className={style.boxchapter}>
     <span className={style.currentChapter}>
       Current Chapter
     </span>
-    <span className={style.currentChapter}>
+    <span className={style.chapter}>
       Chapter 17
     </span>
-    <button className="btn btnprimary" type="button"> UPDATE PROGRESS</button>
+    <button className="btn btn-primary" type="button"> UPDATE PROGRESS</button>
   </div>
 );
 
